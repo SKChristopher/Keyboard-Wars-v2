7623a4fbdd79e9fece0ca9a1066bd811b2f50520
Christopher <chris132128@gmail.com>
2018-06-03 22:47:14 -0700

    trailer terminated on close

--- client/components/Trailer.jsx
+++ client/components/Trailer.jsx
@@ -2,32 +2,27 @@ import React from "react";
 
 const Trailer = props => {
   let styling;
-  if (props.watching === true) styling = { display: "block" };
-  else {
-    styling = { display: "none" };
-    // var iframe = document.getElementsByClassName("trailer").contentWindow;
-    // iframe.postMessage('{"event":"command","func":"' + func + '","args":""}', '*');
-    // document.getElementsByClassName("trailer");
-  }
-  return (
-    <div className="trailer-div" style={styling}>
-      <div id="close-button" onClick={props.closeTrailer}>
-        X
+  if (props.watching === true) {
+    return (
+      <div className="trailer-div" style={styling}>
+        <div id="close-button" onClick={props.closeTrailer}>
+          X
+        </div>
+        <iframe
+          className="trailer"
+          width="100%"
+          height="93%"
+          src={
+            props.currentMovie.trailer +
+            "?autoplay=1&showinfo=0&rel=0&controls=0&autohide=1&modestbranding=1"
+          }
+          frameborder="0"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+        />
       </div>
-      <iframe
-        className="trailer"
-        width="100%"
-        height="93%"
-        src={
-          props.currentMovie.trailer +
-          "?autoplay=1&showinfo=0&rel=0&controls=0&autohide=1&modestbranding=1"
-        }
-        frameborder="0"
-        allow="autoplay; encrypted-media"
-        allowfullscreen
-      />
-    </div>
-  );
+    );
+  } else return <div></div>;
 };
 
 export default Trailer;
